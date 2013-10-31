d88a8c3ea926468fd46a36fed78ea35bcfd87dee
Rob Brambley <rrbrambley@gmail.com>
2013-10-31 12:27:13 -0700

    add category for getting annotation values from messages easily
    e.g. ohai displaydate

---
+++ ADNKitUtils/ANKMessage+AATTAnnotationHelper.h
@@ -0,0 +1,13 @@
+//
+//  ANKMessage+AATTAnnotationHelper.h
+//  ADNKitUtils
+//
+//  Created by Rob Brambley on 10/31/13.
+//  Copyright (c) 2013 Always All The Time. All rights reserved.
+//
+
+@interface ANKMessage (AATTAnnotationHelper)
+
+- (NSDate *)ohaiDisplayDate;
+
+@end
---
+++ ADNKitUtils/ANKMessage+AATTAnnotationHelper.m
@@ -0,0 +1,24 @@
+//
+//  ANKMessage+AATTAnnotationHelper.m
+//  ADNKitUtils
+//
+//  Created by Rob Brambley on 10/31/13.
+//  Copyright (c) 2013 Always All The Time. All rights reserved.
+//
+
+#import <ANKValueTransformations.h>
+
+#import "ANKMessage+AATTAnnotationHelper.h"
+
+@implementation ANKMessage (AATTAnnotationHelper)
+
+- (NSDate *)ohaiDisplayDate {
+    NSArray *annotations = [self annotationsWithType:@"net.app.ohai.displaydate"];
+    if([annotations count] > 0) {
+        ANKAnnotation *annotation = [annotations objectAtIndex:0];
+        return [[ANKValueTransformations transformations] NSDateFromNSString:[[annotation value] objectForKey:@"date"]];
+    }
+    return nil;
+}
+
+@end
