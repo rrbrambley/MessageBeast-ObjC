c73c59c503615d905b1f754a929bca9e0ddb49c9
Rob Brambley <rrbrambley@gmail.com>
2013-11-03 10:52:38 -0800

    insert hashtag instances right after saving a message

--- ADNKitUtils/AATTMessageManager.m
+++ ADNKitUtils/AATTMessageManager.m
@@ -192,6 +192,7 @@ static CLGeocoder *geocoder;
     
     if(self.configuration.isDatabaseInsertionEnabled) {
         [self.database insertOrReplaceMessage:messagePlus];
+        [self.database insertOrReplaceHashtagInstances:messagePlus];
     }
 }
 
