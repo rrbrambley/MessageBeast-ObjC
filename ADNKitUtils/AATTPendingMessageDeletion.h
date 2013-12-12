042dcf15068b2da1c643c59cf91cfb18d2aabc6c
Rob Brambley <rrbrambley@gmail.com>
2013-12-12 14:44:40 -0800

    add AATTPendingMessageDeletion

---
+++ ADNKitUtils/AATTPendingMessageDeletion.h
@@ -0,0 +1,13 @@
+//
+//  AATTPendingMessageDeletion.h
+//  ADNKitUtils
+//
+//  Created by Rob Brambley on 12/12/13.
+//  Copyright (c) 2013 Always All The Time. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AATTPendingMessageDeletion : NSObject
+
+@end
---
+++ ADNKitUtils/AATTPendingMessageDeletion.m
@@ -0,0 +1,13 @@
+//
+//  AATTPendingMessageDeletion.m
+//  ADNKitUtils
+//
+//  Created by Rob Brambley on 12/12/13.
+//  Copyright (c) 2013 Always All The Time. All rights reserved.
+//
+
+#import "AATTPendingMessageDeletion.h"
+
+@implementation AATTPendingMessageDeletion
+
+@end
