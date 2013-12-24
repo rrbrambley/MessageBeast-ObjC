22e5d8029761fd40163de1326e77abe3394941e1
Rob Brambley <rrbrambley@gmail.com>
2013-12-23 23:09:41 -0800

    Add AATTChannelSpec

---
+++ ADNKitUtils/AATTChannelSpec.h
@@ -0,0 +1,16 @@
+//
+//  AATTChannelSpec.h
+//  ADNKitUtils
+//
+//  Created by Rob Brambley on 12/23/13.
+//  Copyright (c) 2013 Always All The Time. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AATTChannelSpec : NSObject
+
+@property NSString *type;
+@property NSDictionary *queryParameters;
+
+@end
---
+++ ADNKitUtils/AATTChannelSpec.m
@@ -0,0 +1,13 @@
+//
+//  AATTChannelSpec.m
+//  ADNKitUtils
+//
+//  Created by Rob Brambley on 12/23/13.
+//  Copyright (c) 2013 Always All The Time. All rights reserved.
+//
+
+#import "AATTChannelSpec.h"
+
+@implementation AATTChannelSpec
+
+@end
